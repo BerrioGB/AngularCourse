8d4e38841d4197cb81e9f42adf8950f269d09573
carlitos <juan.gomezio@amigo.edu.co>
2023-04-01 21:45:59 -0500

    First Commit With All Content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { movie } from './movie.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,94 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'FirstAngular';
+  name = 'Carlitos'
+  age = '22'
+  city = 'Belen SanBer'
+  img = 'https://trabajosdiarios.com/storage/logo/co/MVMI/kgYU4hVJerIkKZCtkZSj4PHUo6AEAqOnrsMnsmpV.png'
+  btnState = true
+
+  /**Objeto Para el login */
+  registro = {
+    name: '',
+    email:'',
+    password:''
+  }
+  /**Objeto ngStyles */
+  box = {
+    width: 100,
+    height: 100,
+    background: 'red'
+  }
+
+  /**Variable estilos dinamicos */
+  widthImg = 10;
+
+  /* String Para *nfFor*/
+  names: string[] = ['Juan', 'Javier', 'Santiago']
+  newName = ''
+
+
+  /*Trabajando con objetos*/
+  person = {
+    name: 'Antonio',
+    age: 30,
+    city: 'Miami',
+    img: 'https://trabajosdiarios.com/storage/logo/co/MVMI/kgYU4hVJerIkKZCtkZSj4PHUo6AEAqOnrsMnsmpV.png'
+
+  }
+  /** Arreglo de objetos con productos para *nfFor en arreglos*/
+  movies: movie[] = [
+    {
+      name:'pelicula 1',
+      price: 100,
+      portada: "../assets/img/american.jpg"
+    },
+    {
+      name:'pelicula 2',
+      price: 200,
+      portada: "../assets/img/Figth.jpg"
+    },
+    {
+      name:'pelicula 3',
+      price: 300,
+      portada: "../assets/img/inter.jpg"
+    },
+    {
+      name:'pelicula 4',
+      price: 400,
+      portada: "../assets/img/pulp.jpg"
+    },
+    {
+      name:'pelicula 5',
+      price: 500,
+      portada: "../assets/img/rocky.jpg"
+    }
+  ]
+
+  /*Primer Metodo*/
+
+  toggleButton() {
+    this.btnState = !this.btnState
+  }
+  ageInc() {
+    this.person.age += 1
+  }
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement
+    console.log(element.scrollTop)
+  }
+
+  onKey(event: Event) {
+    const element = event.target as HTMLInputElement
+    this.person.name = element.value
+
+  }
+  newNom() {
+    this.names.push(this.newName)
+    this.newName = ''
+  }
+  deleteName(index: number) {
+    this.names.splice(index, 1)
+  }
+
 }
